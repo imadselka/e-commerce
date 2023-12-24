77aa6b17d98706c1cae7a30a398f68f028c97f0f
Imad Selka <imadselkawi133@gmail.com>
2023-12-24 21:01:29 +0100

    First commit

--- src/app/_components/Input/index.tsx
+++ src/app/_components/Input/index.tsx
@@ -25,35 +25,37 @@ export const Input: React.FC<Props> = ({
   disabled,
 }) => {
   return (
-    <div className={classes.inputWrap}>
-      <label htmlFor="name" className={classes.label}>
-        {label}
-        {required ? <span className={classes.asterisk}>&nbsp;*</span> : ''}
-      </label>
-      <input
-        className={[classes.input, error && classes.error].filter(Boolean).join(' ')}
-        {...{ type }}
-        {...register(name, {
-          required,
-          validate,
-          ...(type === 'email'
-            ? {
-                pattern: {
-                  value: /\S+@\S+\.\S+/,
-                  message: 'Please enter a valid email',
-                },
-              }
-            : {}),
-        })}
-        disabled={disabled}
-      />
-      {error && (
-        <div className={classes.errorMessage}>
-          {!error?.message && error?.type === 'required'
-            ? 'This field is required'
-            : error?.message}
-        </div>
-      )}
-    </div>
+    <>
+      <div className={classes.inputWrap}>
+        <label htmlFor="name" className={classes.label}>
+          {label}
+          {required ? <span className={classes.asterisk}>&nbsp;*</span> : ''}
+        </label>
+        <input
+          className={[classes.input, error && classes.error].filter(Boolean).join(' ')}
+          {...{ type }}
+          {...register(name, {
+            required,
+            validate,
+            ...(type === 'email'
+              ? {
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: 'Please enter a valid email',
+                  },
+                }
+              : {}),
+          })}
+          disabled={disabled}
+        />
+        {error && (
+          <div className={classes.errorMessage}>
+            {!error?.message && error?.type === 'required'
+              ? 'This field is required'
+              : error?.message}
+          </div>
+        )}
+      </div>
+    </>
   )
 }
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ import path from 'path'
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
 })
-
 import express from 'express'
 import payload from 'payload'
 
